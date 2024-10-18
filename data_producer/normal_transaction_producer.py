48741d95bd609f8620adcfb606789049aefcd0b2
NazzX1 <triomono555@gmail.com>
2024-10-18 22:00:38 +0100

    wip

---
+++ data_producer/normal_transaction_producer.py
@@ -0,0 +1,44 @@
+import time
+import random
+from kafka import KafkaProducer
+import json
+import numpy as np
+from datetime import datetime, timedelta
+
+
+
+class TransactionProducer:
+    def __init__(self, brokers, topic):
+        self.producer = KafkaProducer(
+            bootstrap_servers = brokers,
+            value_serializer = lambda v: json.dumps(v).encode('utf-8')
+        )
+        self.topic = topic
+
+
+
+        def produce_transaction(self):
+            while True:
+                transaction = {
+                    'distance_from_home': np.random.uniform(0, 0.5),
+                    'distance_from_last_transaction' : np.random.uniform(0, 40),
+                    'ratio_to_median_purchase_price': np.random.uniform(0, 0.5),
+                    'repeat_retailer': np.random.choice([0,1]),
+                    'used_chip': np.random.choice([0,1]),
+                    'used_pin_number': np.random.choice([0,1]),
+                    'online_order': np.random.choice([0,1]),
+                }
+
+                #Send transaction data to kafka topic
+                self.producer.send(self.topic, value = transaction)
+                print(f'Produced: {transaction}')
+                
+                time.sleep(1)
+
+
+if __name__ == '__main__':
+    brokers = ['broker:29092']
+    topic = 'raw_transaction'
+
+    producer = TransactionProducer(brokers, topic)
+    producer.produce_transaction()
