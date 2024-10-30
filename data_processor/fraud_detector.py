b39808d52a10b936ba4cab143ff348b569c4a3df
NazzX1 <triomono555@gmail.com>
2024-10-30 20:19:45 +0100

    wip

--- data_processor/fraud_detector.py
+++ data_processor/fraud_detector.py
@@ -52,3 +52,48 @@ with open('fraud_detection.pkl', 'rb') as model:
     model = pickle.load(model)
 
 
+def detect_fraud(dfh, dflt, rmpp, repeat_retailer, used_chip, used_pin_number, online_order):
+
+    transaction = {
+                    'distance_from_home': dfh,
+                    'distance_from_last_transaction' : dflt,
+                    'ratio_to_median_purchase_price': rmpp,
+                    'repeat_retailer': repeat_retailer,
+                    'used_chip': used_chip,
+                    'used_pin_number': used_pin_number,
+                    'online_order': online_order, 
+                }
+    
+    model_features = ['distance_from_home', 'distance_from_last_transaction',
+                      'ratio_to_median_purchase_price', 'repeat_retailer',
+                      'used_chip', 'used_pin_number', 'online_order']
+    
+
+    transaction_df = pd.DataFrame([transaction])
+
+    is_fraud = model.predict(transaction_df)[0]
+
+    return bool(is_fraud) 
+
+
+
+transaction_stream = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "broker:29092") \
+    .option("subscribe", "transactions") \
+    .load()
+
+
+
+transaction_data = transaction_stream.selectExpr("CAST(value AS STRING)")
+transaction_json = transaction_data.select(from_json(col("value"), schema).alias("transaction_data"))
+
+
+
+
+transaction_df = transaction_json.select("transaction_data.*")
+
+
+
+
